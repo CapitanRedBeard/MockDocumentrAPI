3505d56c0cfb236a93809efe301f6bf6dd5f3069
Emmett Harper <eharpe23@yahoo.com>
2017-10-30 21:50:41 -0700

    using documentr plugin

--- lib/example_project/logger.ex
+++ lib/example_project/logger.ex
@@ -1,26 +1,26 @@
-defmodule Documentr.Logger do
-  use HTTPotion.Base
-
-  def process_url(url) do
-    "http://localhost:4000/v1/api/path"
-  end
-
-  def process_request_headers(headers) do
-    Dict.put [Accepts: "application/json", "Content-Type": "application/json"], :"User-Agent", "github-potion"
-  end
-
-  def post(body), do: request(:post, "", turn_body_to_options(body))
-
-  defp turn_body_to_options(body) do
-    path_params = %{
-      "path" => Map.merge(%{
-        "api_key" => "794190d0-bd7e-11e7-977d-dca90475b70a",
-        "response_type" => "application/json",
-        "request_type" => "application/json"
-       }, body)
-    }
-
-    [body: to_string Poison.Encoder.encode(path_params,[])]
-  end
-
-end
+# defmodule Documentr.Logger do
+#   use HTTPotion.Base
+#
+#   def process_url(url) do
+#     "http://localhost:4000/v1/api/path"
+#   end
+#
+#   def process_request_headers(headers) do
+#     Dict.put [Accepts: "application/json", "Content-Type": "application/json"], :"User-Agent", "github-potion"
+#   end
+#
+#   def post(body), do: request(:post, "", turn_body_to_options(body))
+#
+#   defp turn_body_to_options(body) do
+#     path_params = %{
+#       "path" => Map.merge(%{
+#         "api_key" => "794190d0-bd7e-11e7-977d-dca90475b70a",
+#         "response_type" => "application/json",
+#         "request_type" => "application/json"
+#        }, body)
+#     }
+#
+#     [body: to_string Poison.Encoder.encode(path_params,[])]
+#   end
+#
+# end
--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule ExampleProject.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:httpotion, "~> 3.0.2"}]
+     {:httpotion, "~> 3.0.2"},
+     {:documentr, "~> 0.1.0"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
